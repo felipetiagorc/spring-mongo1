42b23448ac61df5a1b08a1d22ea418c02e74d53f
Felipe Pereira <felipetiagorc@yahoo.com.br>
2020-10-16 15:02:04 -0300

    projeto criado

---
+++ src/test/java/com/felipe/springmongo1/SpringMongo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.springmongo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
