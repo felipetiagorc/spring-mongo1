822a7ec2a5c370b841f00f1b9ba93f8cd1fe9601
Felipe <felipetiagorc@yahoo.com.br>
2020-10-28 20:41:15 -0300

    referenciando posts do user com @DBRef

--- src/main/java/com/felipe/springmongo1/domain/User.java
+++ src/main/java/com/felipe/springmongo1/domain/User.java
@@ -1,8 +1,11 @@
 package com.felipe.springmongo1.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -13,6 +16,11 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	// cria a Lista e ja instancia com new ArrayList (lembrando q no java a Lista é apenas uma Interface e ArrayList é uma implementação possível dessa Lista.
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	
 	public User() {}
 
@@ -45,6 +53,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
