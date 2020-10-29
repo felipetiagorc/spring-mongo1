60b4b8c89209ecae309a43d9e070d24b53969e15
Felipe <felipetiagorc@yahoo.com.br>
2020-10-28 22:05:21 -0300

    acrescentando comments aos Posts

--- src/main/java/com/felipe/springmongo1/config/Instantiation.java
+++ src/main/java/com/felipe/springmongo1/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.felipe.springmongo1.domain.Post;
 import com.felipe.springmongo1.domain.User;
 import com.felipe.springmongo1.dto.AuthorDTO;
+import com.felipe.springmongo1.dto.CommentDTO;
 import com.felipe.springmongo1.repository.PostRepository;
 import com.felipe.springmongo1.repository.UserRepository;
 
@@ -35,17 +36,23 @@ public class Instantiation implements CommandLineRunner{
 		
 		
 		User maria = new User(null, "Maria das Dores", "mdores@hg.com");
-		User jose = new User(null, "Jose Silva", "jose@yaho.com");
+		User alex = new User(null, "Alex Silva", "alex@yaho.com");
 		User bob = new User(null, "Bob Marli", "bob@marli.com");
 		
-		userRepository.saveAll(Arrays.asList(maria, jose, bob));
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		
 		// instanciando Posts, já associado com o autor deles.. ultimo param: 
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pra SP abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/04/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 		
-
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		
--- src/main/java/com/felipe/springmongo1/domain/Post.java
+++ src/main/java/com/felipe/springmongo1/domain/Post.java
@@ -1,12 +1,15 @@
 package com.felipe.springmongo1.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.felipe.springmongo1.dto.AuthorDTO;
+import com.felipe.springmongo1.dto.CommentDTO;
 
 
 @Document
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {}
 
@@ -71,6 +76,16 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -95,6 +110,7 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
+
 
 
 	
