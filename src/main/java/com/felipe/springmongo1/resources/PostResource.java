bdbfd81b5521c014436da7fdd970fbf47890a26b
Felipe <felipetiagorc@yahoo.com.br>
2020-10-28 21:32:39 -0300

    Criando Post-Service e Resource e get por Id

---
+++ src/main/java/com/felipe/springmongo1/resources/PostResource.java
@@ -0,0 +1,33 @@
+ package com.felipe.springmongo1.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felipe.springmongo1.domain.Post;
+import com.felipe.springmongo1.services.PostService;
+
+
+@RestController 
+@RequestMapping(value="/posts") 
+
+public class PostResource {
+	@Autowired
+	private PostService service;
+	
+	//  endpoint para buscar usuario por id:
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		// instancia obj usuario recebendo service.findById(id):
+		Post obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj); 
+		
+	}
+	
+	
+
+} 
---
+++ src/main/java/com/felipe/springmongo1/services/PostService.java
@@ -0,0 +1,23 @@
+package com.felipe.springmongo1.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipe.springmongo1.domain.Post;
+import com.felipe.springmongo1.repository.PostRepository;
+import com.felipe.springmongo1.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		// se não existir usuário com o id passado acima, vai retornar nulo então precisa tratar:
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+		}
+}
